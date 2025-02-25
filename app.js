bad14d397b06cb693c63be3035c743ce50eef5ff
Abdulqadir75434 <aq987654325@gmail.com>
2025-02-25 08:36:01 -0800

    commit 2

---
+++ app.js
@@ -0,0 +1,15 @@
+//chapter 1
+//Question 1
+alert("Hello' Walcome to my web");
+//question 2
+console.log(" walcom to my website");
+//Question 3
+document.write("Hello, World!<br>");
+document.write("This is a new line.<br>");
+document.write("Using line breaks in HTML.");
+//Question 4
+alert("Message 1: Hello!");
+alert("Message 2: How are you?");
+alert("Message 3: Welcome to our website!");
+//Question 5
+console.log("Hello! MY name is AbdulQadir I am student of SMIT-Banch-15");
