8de4795a33320ed5a2806421fc721bac03ac78e6
Abdulqadir75434 <aq987654325@gmail.com>
2025-02-25 12:59:11 -0800

    commit 3

--- app.js
+++ app.js
@@ -4,12 +4,29 @@ alert("Hello' Walcome to my web");
 //question 2
 console.log(" walcom to my website");
 //Question 3
-document.write("Hello, World!<br>");
-document.write("This is a new line.<br>");
-document.write("Using line breaks in HTML.");
+console.log("Hello, World! \n This is a new line. \nUsing line breaks in HTML.")
 //Question 4
 alert("Message 1: Hello!");
 alert("Message 2: How are you?");
 alert("Message 3: Welcome to our website!");
 //Question 5
 console.log("Hello! MY name is AbdulQadir I am student of SMIT-Banch-15");
+
+
+
+
+
+
+
+//chapter 2
+//Question 1
+var username;
+//Question 2
+var username="AbdlulQadir";   
+//Question 3
+var message;
+message ="Hello world";
+alert(message)
+//Question 4
+var Message = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(Message);
