b6d43edbc7dddc57aef0c7871fc80ac6ee8d7322
Abdulqadir75434 <aq987654325@gmail.com>
2025-02-26 03:57:22 -0800

    commit 4

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-//chapter 1
+//chapter 1 
 //Question 1
-alert("Hello' Walcome to my web");
+//alert("Hello' Walcome to my web");
 //question 2
-console.log(" walcom to my website");
+//alert("Error! Place");
 //Question 3
 console.log("Hello, World! \n This is a new line. \nUsing line breaks in HTML.")
 //Question 4
-alert("Message 1: Hello!");
-alert("Message 2: How are you?");
-alert("Message 3: Welcome to our website!");
+//alert("Message 1: Hello!");
+//alert("Message 2: How are you?");
+//alert("Message 3: Welcome to our website!");
 //Question 5
 console.log("Hello! MY name is AbdulQadir I am student of SMIT-Banch-15");
 
@@ -30,3 +30,46 @@ alert(message)
 //Question 4
 var Message = "PIZZA\nPIZZ\nPIZ\nPI\nP";
 alert(Message);
+
+
+
+
+
+
+
+
+
+
+
+
+
+//chapter 3
+// Question 4
+// var username= prompt("Enter your name")
+// var product =prompt("Enter your product name")
+// var Quantity =prompt("Enter your product Quantity")
+
+// document.write(username + " ORDERD " + Quantity + "  " + product + "  " + " LUNDA BAZAR ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//chapter-4
+
+var celsiustemperature = prompt("enter a celsiustemperature") 
+var fahrenheit =(celsiustemperature * 9/5) + 32;
+document.write(celsiustemperature + "celsius is equal to" + fahrenheit )
+
+var fahrenheit = prompt("enter a fahrenheit temperature") 
+var celsiustemperature =(fahrenheit - 32)*5/9;
+document.write(fahrenheit + "fahrenheit is equal to" + celsiustemperature  )
