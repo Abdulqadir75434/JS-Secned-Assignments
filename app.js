07dbb8741808e99b266dac946c20e6b88f1d0fdf
Abdulqadir75434 <aq987654325@gmail.com>
2025-02-27 13:29:33 -0800

    commit 8

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ console.log("Hello, World! \n This is a new line. \nUsing line breaks in HTML.")
 //alert("Message 3: Welcome to our website!");
 //Question 5
 console.log("Hello! MY name is AbdulQadir I am student of SMIT-Banch-15");
+
+
 //chapter 2
-
-
 //Question 1
 var username;
 //Question 2
@@ -22,9 +22,6 @@ var username="AbdlulQadir";
 var message;
 message ="Hello world";
 alert(message)
-
-
-
 //Question 4
 var Studentname = "Abdul qadir ";
 var Studentage = 16;
@@ -38,54 +35,107 @@ var institute = "SMIT Institute";
 //alert("Institute: " + institute);
 //Quastion 5
 var Message = "PIZZA\nPIZZ\nPIZ\nPI\nP";
-alert(Message);
+//alert(Message);
 //Question 6
 var email = ("aq2929738@gmail.com")
 //alert("My email address is " + email);
 //Question 7
-var book = "A smarter way to learn JavaScript";
-alert("I am currently reading " + book);
+var book = ("A smarter way to learn JavaScript");
+//alert("I am currently reading " + book);
 //Question 8
-document.write("Hello, this is displayed in the browser using JavaScript!"); <br/>
+document.write("Hello, this is displayed in the browser using JavaScript! <br>");
 //Question 9
-var  alertmessage = "Hello! This is a JavaScript message.";
-//alert(message);
-document.write(message);
-
-
-
-
-
-
+var alertmessage = ("Hello! This is a JavaScript message.");
+//alert(alertmessage);
+document.write(alertmessage +"<br>");
 
 //chapter 3
 //Question 1
 var userage= ("16");
 alert("My age is " + userage);
+//Question 2
 
-
+//Question 3
+var birthYear = 2008; 
+document.write("My birth year is " + birthYear + "<br>");
 // Question 4
-// var username= prompt("Enter your name")
-// var product =prompt("Enter your product name")
-// var Quantity =prompt("Enter your product Quantity")
+//var username= prompt("Enter your name")
+//var product =prompt("Enter your product name")
+//var Quantity =prompt("Enter your product Quantity")
 
-// document.write(username + " ORDERD " + Quantity + "  " + product + "  " + " LUNDA BAZAR ")
-
-
-
-
-
-
-
-
-
-
-
-
+//document.write(username + " ORDERD " + Quantity + "  " + product + "  " + " LUNDA BAZAR ")
 
 
 //chapter-4
 
+//Question 1
+var x = 10, y = 20, z = 30;
+//Question 2
+// Legal Variable Names
+let myName = "Ali";     
+let _age = 25;           
+let $price = 100;        
+let user123 = "Ahmed";   
+let camelCaseExample = "Hello";
+//Illegal Variable Names
+//let 123name = "John";    
+//let my-name = "Error";  
+//let let = "Variable";   
+//let user name = "Ali";   
+//let @age = 30;        
+
+//Question 3
+//document.write("<h1>Rules for naming JS variables</h1>");
+//document.write("<p>Variable names can only contain <b>letters</b>, <b>numbers</b>, <b>underscore (_)</b>, and <b>dollar sign ($)</b>.</p>");
+//document.write("<p>For example: <code>$my_1stVariable</code></p>");
+
+//document.write("<p>Variables must begin with a <b>letter</b>, <b>underscore (_)</b>, or <b>dollar sign ($)</b>.</p>");
+//document.write("<p>For example: <code>$name, _name, name</code></p>");
+
+//document.write("<p>Variable names are case <b>sensitive</b>.</p>");
+//document.write("<p>Variable names should not be JS <b>reserved keywords</b>.</p>");
+
+
+//chapter 5
+//Question 1
+// var num1 = +prompt("Enter first number:");
+// var num2 = +prompt("Enter second number:");
+
+// var sum = num1 + num2;
+// document.write("<h2>Sum of " + num1 + " and " + num2 + " is " + sum + ".</h2>");
+
+//Question 2
+// var num1 = prompt("Enter first number:");
+// var num2 = prompt("Enter second number:");
+
+// var sum = num1 + num2;
+// var subtraction = num1 - num2;
+// var multiplication = num1 * num2;
+// var division = num1 / num2;
+// var modulus = num1 % num2;
+
+// document.write("<h2>Results:</h2>");
+// document.write("<p>Sum: " + num1 + " + " + num2 + " = " + sum + "</p>");
+// document.write("<p>Subtraction: " + num1 + " - " + num2 + " = " + subtraction + "</p>");
+// document.write("<p>Multiplication: " + num1 + " * " + num2 + " = " + multiplication + "</p>");
+// document.write("<p>Division: " + num1 + " / " + num2 + " = " + division + "</p>");
+// document.write("<p>Modulus: " + num1 + " % " + num2 + " = " + modulus + "</p>");
+
+//Question 5
+var num = +prompt('enter number');
+
+ document.write(num + ' * 1 = ' +  num * 1 + "<br>");
+ document.write(num + ' * 2 = ' +  num * 2 + "<br>");
+ document.write(num + ' * 3 = ' +  num * 3 + "<br>");
+ document.write(num + ' * 4 = ' +  num * 4 + "<br>");
+ document.write(num + ' * 5 = ' +  num * 5 + "<br>");
+ document.write(num + ' * 6 = ' +  num * 6 + "<br>");
+ document.write(num + ' * 7 = ' +  num * 7 + "<br>");
+ document.write(num + ' * 8 = ' +  num * 8 + "<br>");
+ document.write(num + ' * 9 = ' +  num * 9 + "<br>");
+ document.write(num + ' * 10 = ' +  num * 10 + "<br>");
+
+//Question 6
 // var celsiustemperature = prompt("enter a celsiustemperature") 
 // var fahrenheit =(celsiustemperature * 9/5) + 32;
 // document.write(celsiustemperature + "celsius is equal to" + fahrenheit )
